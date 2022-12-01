8e8a7f697e404f5492d8d5be2057b9fdbaa068a5
Felipe <felipedossantosflor@gmail.com>
2022-12-01 10:21:52 -0300

    @FelipeFlor [Task: First commit]

---
+++ LivroCaixaTeste/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LivroCaixaTeste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
