9068655fedbf75b17bdf4c8e15e508cf12fb216b
Felipe <felipedossantosflor@gmail.com>
2022-12-01 16:00:11 -0300

    @FelipeFlor [Task: Criação camada modelo]

---
+++ LivroCaixaAPI/Src/Modelos/LancamentoModelo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LivroCaixaAPI.Src.Modelos
+{
+    /// <summary>
+    /// <para>Resumo: Classe responsavel por representar tb_postagens no banco.</para>
+    /// <para>Criado por: Felipe Flor</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 01/12/2022</para>
+    /// </summary>
+    [Table("tb_lancamentos")]
+    public class Lancamento
+    {
+        #region Atributos
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime Date { get; set;  }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public enum TipoLancamento
+        {
+            Receita,
+            Despesa
+        }
+        public bool Recorrencia { get; set; }
+
+        [ForeignKey("fk_usuario")]
+        public Usuario Criador { get; set; }
+        #endregion
+    }
+}
---
+++ LivroCaixaAPI/Src/Modelos/UsuarioModelo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace LivroCaixaAPI.Src.Modelos
+{
+    /// <summary>
+    /// <para>Resumo: Classe responsavel por representar tb_usuarios no banco. </para>
+    /// <para>Criado por: Felipe Flor</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 01/12/2022</para>
+    /// </summary>
+    [Table("tb_usuarios")]
+    public class Usuario
+    {
+        #region Atributos
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        [JsonIgnore, InverseProperty("Criador")]
+        public List<Lancamento> MeusLancamentos { get; set; }
+
+        #endregion
+    }
+}
